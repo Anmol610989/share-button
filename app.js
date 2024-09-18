e61e0de996da240e48b6292a7c697485984f74ac
Anmol610989 <67320791+Anmol610989@users.noreply.github.com>
2024-09-18 16:01:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const share_button = document.querySelector('.share-button');
+
+share_button.addEventListener('click', ()=>{
+    if(share_button.value = 'checked'){
+        document.querySelector('.share-icons').style.display = 'flex';
+    }
+})
+
+
+
